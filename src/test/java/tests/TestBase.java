c2479f700374cb90e81b17dc853afc3dc5a7f64f
astelmakh13 <stelmakh.al@gmail.com>
2024-03-28 17:05:25 -0500

    removed unused imports

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -10,8 +10,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-
 public class TestBase {
 
 
@@ -20,7 +18,7 @@ public class TestBase {
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("version", "122.0");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browserSize = System.getProperty("resolution", "1920x1080");
         Configuration.remote = "https://user1:1234@" + System.getProperty("remote", "selenoid.autotests.cloud") + "/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
@@ -39,7 +37,7 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
-        closeWebDriver();
+
 
 
     }
